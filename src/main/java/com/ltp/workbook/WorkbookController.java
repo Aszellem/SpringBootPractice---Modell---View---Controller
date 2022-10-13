b5c50d3ba739a01d5a05de3f9a37e35ab544fd8b
aszellem <aszellemek@gmail.com>
2022-10-13 17:58:22 +0200

    update return

--- src/main/java/com/ltp/workbook/WorkbookController.java
+++ src/main/java/com/ltp/workbook/WorkbookController.java
@@ -4,14 +4,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WorkbookController {
 
     @GetMapping("/")
-    public String getShows(){
+    public ModelAndView getShows(){
         Map<String, Show> model = new HashMap<String, Show>();        
         model.put("first", new Show("Breaking Bad", "Ozymandias", 10.0));
         model.put("second", new Show("Attack on Titan", "Hero", 9.9));
@@ -19,6 +19,6 @@ public class WorkbookController {
         model.put("fourth", new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9));
         model.put("fifth", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
 
-        return "shows";
+        return new ModelAndView("shows", model);
     }
 }
