651fba05c879ccc1d6076dbdc1170331a1e9c72c
aszellem <aszellemek@gmail.com>
2022-10-13 17:55:05 +0200

    Create a HashMap store key-value pairs

--- src/main/java/com/ltp/workbook/WorkbookController.java
+++ src/main/java/com/ltp/workbook/WorkbookController.java
@@ -1,5 +1,8 @@
 package com.ltp.workbook;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WorkbookController {
 
     @GetMapping("/")
-    public String getShows(Model model){        
-        model.addAttribute("first", new Show("Breaking Bad", "Ozymandias", 10.0));
-        model.addAttribute("second", new Show("Attack on Titan", "Hero", 9.9));
-        model.addAttribute("third", new Show("Attack on Titan", "Perfect Game", 9.9));
-        model.addAttribute("fourth", new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9));
-        model.addAttribute("fifth", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
+    public String getShows(){
+        Map<String, Show> model = new HashMap<String, Show>();        
+        model.put("first", new Show("Breaking Bad", "Ozymandias", 10.0));
+        model.put("second", new Show("Attack on Titan", "Hero", 9.9));
+        model.put("third", new Show("Attack on Titan", "Perfect Game", 9.9));
+        model.put("fourth", new Show("Star Wars: The Clone Wars", "Victory and Death", 9.9));
+        model.put("fifth", new Show("Mr. Robot", "407 Proxy Authentication Required", 9.9));
 
         return "shows";
     }
